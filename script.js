f8f8af7727a5e6a9e86c8ff0d676900a00743aff
18_genki_kanda <137843668+genki-11@users.noreply.github.com>
2023-08-28 15:26:46 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict';
+// please do not delete the 'use strict' line above
+
+document.getElementById('color-button').addEventListener('click', changeColor);
+document.getElementById('be-exorbitant').addEventListener('click', getAnimal);
+document.getElementById('back-ground-button').addEventListener('click', changeBackGround);
+
+function changeColor() {
+  console.log('Button clicked!'); // feel free to change/delete this line
+  document.body.style.backgroundColor = `#FFC0CB`;
+  document.getElementById(`color-button`).style.backgroundColor = `green`;
+}
+
+function getAnimal() {
+  document.getElementById(`be-exorbitant`).style.backgroundColor = `red`;
+  alert("ウキー！ウキキウキー！ウキ？🤔");
+  let outPutAnimal = document.getElementById("text-animal"); 
+  outPutAnimal.innerHTML += "🙈🙉🙊🙈🙉🙊";
+}
+
+function changeBackGround() {
+  document.body.style.backgroundImage = "url(images/1.jpg)";
+  document.getElementById(`back-ground-button`).style.backgroundColor = `#FFCCFF`;
+}
